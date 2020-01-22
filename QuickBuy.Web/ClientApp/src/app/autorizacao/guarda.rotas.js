c009c1c38736505f403a40fa1cfe7cf2fcc128a6
leonardopianta <leonardo.pianta@dbs-digital.com>
2020-01-22 17:35:51 -0300

    . Alteração

---
+++ QuickBuy.Web/ClientApp/src/app/autorizacao/guarda.rotas.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var GuardaRotas = /** @class */ (function () {
+    function GuardaRotas() {
+    }
+    GuardaRotas.prototype.canActivate = function (route, state) {
+        //Se usuário autenticado, retorna verdadeiro
+        return true;
+    };
+    return GuardaRotas;
+}());
+exports.GuardaRotas = GuardaRotas;
+//# sourceMappingURL=guarda.rotas.js.map
---
+++ QuickBuy.Web/ClientApp/src/app/autorizacao/guarda.rotas.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+
+@Injectable({
+    providedIn:'root'
+})
+
+export class GuardaRotas implements CanActivate {   
+
+    constructor(private router: Router) {
+
+    }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+        //this.router.navigate(["/entrar"]);
+        //Se usuário autenticado, retorna verdadeiro
+        return true;
+    }
+}
